c1dc5e454dd793fab252f6ee0bbc63d300fedef4
nene <59467262+Rinaex@users.noreply.github.com>
2024-10-15 09:07:13 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,15 +1,11 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '前端导航', link: '/nav/' },
-  { text: '茂茂主页', link: 'https://fe-mm.com' },
+  { text: '君铭导航', link: '/nav/' },
+  { text: 'GAL导航', link: '/gal/' },
   {
-    text: '茂茂物语',
-    link: 'https://notes.fe-mm.com',
+    text: '加入群聊',
+    link: '/test',
   },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
-  },
+  { text: '赞助作者', link: '/test' },
 ]
