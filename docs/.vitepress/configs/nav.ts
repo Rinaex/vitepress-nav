2d8b25865d60db6ecd773b4b8dded89b34e89b75
nene <59467262+Rinaex@users.noreply.github.com>
2024-10-15 20:39:23 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '君铭导航', link: '/nav/' },
+  { text: '小铭导航', link: '/nav/' },
   { text: 'GAL导航', link: '/gal/' },
   {
     text: '加入群聊',
